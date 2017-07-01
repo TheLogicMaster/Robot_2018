c00c7b0874a9bec19343d525ef9b2e50c844e671
LogicMaster63 <justinmarentette11@gmail.com>
2017-07-01 05:34:09 -0400

    First commit
    Please work this time

---
+++ org/usfirst/frc/team2832/Robot.java
@@ -0,0 +1,31 @@
+package org.usfirst.frc.team2832;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot{
+
+    @Override
+    public void robotInit() {
+        System.out.println("I'm alive!");
+    }
+
+    @Override
+    public void autonomousInit() {
+
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+
+    }
+
+    @Override
+    public void teleopInit() {
+
+    }
+
+    @Override
+    public void teleopPeriodic() {
+
+    }
+}
